57e83d77fbd96dba49d408f25b24dda59a57c656
vhollo <vhollo@protonmail.com>
2020-05-09 23:32:59 +0200

    .

--- src/routes/dics/[slug].svelte
+++ src/routes/dics/[slug].svelte
@@ -27,62 +27,6 @@
 	};
 </script>
 
-<style>
-	/*
-		By default, CSS is locally scoped to the component,
-		and any unused styles are dead-code-eliminated.
-		In this page, Svelte can't know which elements are
-		going to appear inside the {{{post.html}}} block,
-		so we have to use the :global(...) modifier to target
-		all elements inside .content
-	*/
-	.content :global(h2) {
-		font-size: 1.4em;
-		font-weight: 500;
-	}
-
-	.content :global(pre) {
-		background-color: #f9f9f9;
-		box-shadow: inset 1px 1px 5px rgba(0,0,0,0.05);
-		padding: 0.5em;
-		border-radius: 2px;
-		overflow-x: auto;
-	}
-
-	.content :global(pre) :global(code) {
-		background-color: transparent;
-		padding: 0;
-	}
-
-	.content :global(ul) {
-		line-height: 1.5;
-	}
-
-	.content :global(li) {
-		margin: 0 0 0.5em 0;
-	}
-
-
-	figure {
-		text-align: center;
-	}
-	img {
-		max-width: 100%;
-	}
-	figcaption {
-	}
-
-	aside {
-    display: grid;
-    grid-template-columns: repeat(auto-fill, minmax(30%, 1fr) );
-    grid-gap: 0 2rem;
-	}
-	label {
-    display: flex;
-		justify-content: space-between
-	}
-</style>
-
 <svelte:head>
 	<title>{post.title}</title>
 </svelte:head>
@@ -116,3 +60,61 @@
 <div class='content'>
 	{@html post.html}
 </div>
+
+<style>
+	/*
+		By default, CSS is locally scoped to the component,
+		and any unused styles are dead-code-eliminated.
+		In this page, Svelte can't know which elements are
+		going to appear inside the {{{post.html}}} block,
+		so we have to use the :global(...) modifier to target
+		all elements inside .content
+	*/
+	.content :global(h2) {
+		font-size: 1.4em;
+		font-weight: 500;
+	}
+
+	.content :global(pre) {
+		background-color: #f9f9f9;
+		box-shadow: inset 1px 1px 5px rgba(0, 0, 0, 0.05);
+		padding: 0.5em;
+		border-radius: 2px;
+		overflow-x: auto;
+	}
+
+	.content :global(pre) :global(code) {
+		background-color: transparent;
+		padding: 0;
+	}
+
+	.content :global(ul) {
+		line-height: 1.5;
+	}
+
+	.content :global(li) {
+		margin: 0 0 0.5em 0;
+	}
+
+
+	figure {
+		text-align: center;
+	}
+
+	img {
+		max-width: 100%;
+	}
+
+	figcaption {}
+
+	aside {
+		display: grid;
+		grid-template-columns: repeat(auto-fill, minmax(30%, 1fr));
+		grid-gap: 0 2rem;
+	}
+
+	label {
+		display: flex;
+		justify-content: space-between
+	}
+</style>
