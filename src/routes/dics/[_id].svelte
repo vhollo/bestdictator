a4c05ab9e709ed54f2eb83a25de6bb486f41f21d
vhollo <vhollo@protonmail.com>
2020-05-12 20:08:10 +0200

    .

--- src/routes/dics/[_id].svelte
+++ src/routes/dics/[_id].svelte
@@ -8,7 +8,7 @@
 
 <script>
 	export let post
-	const fieldname = {
+	const fieldnames = {
 		corruption: "Corruption",
 		power_hunger: "Power hunger",
 		agression: "Agression",
@@ -29,38 +29,28 @@
 </svelte:head>
 
 <h1>{post.title}</h1>
+{#if post.desc}
+<h2>{post.desc}</h2>
+{/if}
+
 <p>Date of Birth: <code>{post.birthdate || '?'}</code></p>
 {#if post.isdead}
 <p>Date of Death: <code>{post.deathdate || '?'}</code></p>
 {/if}
 
-<h2>Bio</h2>
-<p>{post.bio}</p>
+<h3>Bio</h3>
+<p>{@html post.bio}</p>
 <figure>
 	<img src="{post.photo}" alt="{post.title}">
-	<figcaption>{post.firstname} {post.middlename} {post.lastname}</figcaption>
+	<figcaption>{post.firstname || ''} {post.middlename || ''} {post.lastname || ''}</figcaption>
 </figure>
 
 {#if post.profile}
 <h2>Profile</h2>
 <aside>
 {#each Object.entries(post.profile) as [key, value]}
-	<label><span>{fieldname[key]}</span> <input type=range min="1" max="5" disabled value={value}></label>
+	<label><span>{fieldnames[key]}</span> <input type=range min="1" max="5" disabled value={value}></label>
 {/each}
-<!-- 
-	<label><span>Psychosis</span><input type=range min="1" max="5" disabled value={post.profile.psychosis}></label>
-	<label><span>Power_hunger</span><input type=range min="1" max="5" disabled value={post.profile.power_hunger}></label>
-	<label><span>Agression</span><input type=range min="1" max="5" disabled value={post.profile.agression}></label>
-	<label><span>Cynicism</span><input type=range min="1" max="5" disabled value={post.profile.cynicism}></label>
-	<label><span>Autocracy</span><input type=range min="1" max="5" disabled value={post.profile.autocracy}></label>
-	<label><span>Telling_lies</span><input type=range min="1" max="5" disabled value={post.profile.telling_lies}></label>
-	<label><span>Bullshit_factor</span><input type=range min="1" max="5" disabled
-			value={post.profile.bullshit_factor}></label>
-	<label><span>Corruption</span><input type=range min="1" max="5" disabled value={post.profile.corruption}></label>
-	<label><span>Megalomania</span><input type=range min="1" max="5" disabled value={post.profile.megalomania}></label>
-	<label><span>Manipulation</span><input type=range min="1" max="5" disabled value={post.profile.manipulation}></label>
-	<label><span>Militancy</span><input type=range min="1" max="5" disabled value={post.profile.militancy}></label>
-	<label><span>Narcissism</span><input type=range min="1" max="5" disabled value={post.profile.narcissism}></label> -->
 </aside>
 {/if}
 
