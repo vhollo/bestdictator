25acbad1d30cacb3d04770e84d0036a994e2b7e1
vhollo <vhollo@protonmail.com>
2020-05-21 02:59:01 +0200

    .

---
+++ src/routes/psyche/index.svelte
@@ -0,0 +1,33 @@
+<script>
+</script>
+
+<style>
+	h1,
+	p {
+		margin: 0 auto;
+	}
+
+	h1 {
+		font-size: 2.8em;
+		font-weight: 700;
+		margin: 0 0 0.5em 0;
+	}
+
+	p {
+		margin: 1em auto;
+	}
+
+	@media (min-width: 480px) {
+		h1 {
+			font-size: 4em;
+		}
+	}
+</style>
+
+<svelte:head>
+	<title>Psyche</title>
+</svelte:head>
+
+<h1>404</h1>
+
+<p>Not found</p>
