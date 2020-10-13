090420ca71a256866a9c4f915e7d4e1c4d4f8bf3
vhollo <vhollo@protonmail.com>
2020-10-14 01:30:14 +0200

    socioimg

--- src/routes/psyche/index.svelte
+++ src/routes/psyche/index.svelte
@@ -10,6 +10,8 @@
 <h1>404</h1>
 
 <p>Not found</p>
+<img src="/_psyche/230b1f78-31a3-4993-8f96-5a61801db8bc.jpg" alt = "ingenius" />
+
 
 
 <style>
@@ -22,9 +24,19 @@
 		margin: 1em auto;
 	}
 
+	img {
+		margin: 0 30% 0 70%;
+		width: 30%;
+	}
+
 	@media (min-width: 480px) {
 		h1 {
 			font-size: 4em;
 		}
+		img {
+			margin : 0 10% 0 10%;
+			width : 80%;
+		}
+
 	}
 </style>
